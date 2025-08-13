27a01e86b102e37acb37d48fd09dce66cab70bca
Patrick McDaniel <dfwjpm@outlook.com>
2025-08-13 09:58:32 -0500

    add ListingFilters to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 
 import { listings as staticListings } from '@/api/data/listings';
+import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
+import { Separator } from '@/components/ui';
 
 const HomePage = () => {
   const [listings, setListings] = useState(staticListings);
 
   return (
     <div className='container py-4'>
+      <div className='mb-4'>
+        <ListingFilters />
+        <Separator className='my-4' />
+      </div>
       <ListingList listings={listings} />
     </div>
   );
